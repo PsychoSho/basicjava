5cdfb10d63af0188bcc25cbd263816c62758b829
Psychosho <chiragduhan2006@gmail.com>
2024-07-19 21:51:35 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    String name;  // Instance variable
+    int age;      // Instance variable
+
+    public void printDetails() {
+        String address = "123 Main St";  // Local variable
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Address: " + address);
+    }
+}
